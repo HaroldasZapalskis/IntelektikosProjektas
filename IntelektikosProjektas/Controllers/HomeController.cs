57cfb05d4d25effeb34c472d3865e104430fae3e
IEUser <IEUser@MSEDGEWIN10>
2018-05-30 16:42:51 -0700

    Initial commit

---
+++ IntelektikosProjektas/Controllers/HomeController.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using IntelektikosProjektas.Models;
+using Microsoft.Extensions.Configuration;
+using MySql.Data.MySqlClient;
+
+namespace IntelektikosProjektas.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IConfiguration configuration;
+
+        public HomeController(IConfiguration config)
+        {
+            this.configuration = config;
+        }
+
+        public IActionResult Index()
+        {
+            string connStr = configuration.GetConnectionString("DefaultConnection");
+
+            MySqlConnection conn = new MySqlConnection(connStr);
+
+            conn.Open();
+
+            MySqlCommand cmd = new MySqlCommand("select * from test", conn);
+
+
+            List<Test> list = new List<Test>();
+            using (var reader = cmd.ExecuteReader())
+            {
+                while (reader.Read())
+                {
+                    list.Add(new Test()
+                    {
+                        Id = Convert.ToInt32(reader["idtest"])
+                    });
+                }
+            }
+
+
+
+            return View(list);
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ IntelektikosProjektas/Models/Test.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IntelektikosProjektas.Models
+{
+    public class Test
+    {
+        public int Id { get; set; }
+    }
+}
